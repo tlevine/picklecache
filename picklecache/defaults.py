938b4167f59ec61b1920857de06a01046da0a3b5
Thomas Levine <_@thomaslevine.com>
2014-04-24 14:47:20 +0000

    -

--- picklecache/defaults.py
+++ picklecache/defaults.py
@@ -1,8 +1,6 @@
-import functools
-
 from pickle_warehouse import Warehouse
 import requests
 
 from picklecache.cache import downloader
 
-get = functools.partial(downloader, lambda x: requests.get(x), Warehouse('get'))
+get = downloader(lambda x: requests.get(x), Warehouse('get'))
