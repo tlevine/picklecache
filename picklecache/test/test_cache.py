ee9010caa94064b7f6bbe7279897153bdf082f0e
Thomas Levine <_@thomaslevine.com>
2014-04-24 14:49:01 +0000

    -

--- picklecache/test/test_cache.py
+++ picklecache/test/test_cache.py
@@ -1,11 +1,12 @@
 from picklecache.cache import downloader
-from picklecache.defaults import get
 
-def test_get_success():
+def test_downloader_success():
     fake_warehouse = {}
     url = 'http://a.b/c'
+    get = downloader(lambda _: int(expected_response), fake_warehouse)
+
     expected_response = 88
-    observed_response = get(fake_warehouse, url, requests_get = lambda _: int(expected_response))
+    observed_response = get(url)
     n.assert_equal(observed_response, expected_response)
     n.assert_tuple_equal(fake_warehouse[url], (None, expected_response))
 
@@ -16,6 +17,7 @@ def test_get_error():
     error = ValueError('This is a test.')
     def fake_get(_):
         raise error
+    get = downloader(fake_get, fake_warehouse)
 
     try:
         get(fake_warehouse, url, requests_get = fake_get)
