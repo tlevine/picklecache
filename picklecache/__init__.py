00a249a685d278830c2e5abcff09c51891ab1eb7
Thomas Levine <_@thomaslevine.com>
2014-05-08 14:48:21 +0000

    python2

--- picklecache/__init__.py
+++ picklecache/__init__.py
@@ -1,3 +1,8 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
-from picklecache.picklecache import cache
+try:
+    # Python 3
+    from picklecache.picklecache import cache
+except ImportError:
+    # Python 2
+    from picklecache import cache
